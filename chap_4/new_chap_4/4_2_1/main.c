04244147dde503b5b5e5ee70f057ee4ca4282902
dongliang Qin <dongliang0908@126.com>
2019-11-22 14:25:45 +0800

    test readline.c

--- chap_4/new_chap_4/4_2_1/main.c
+++ chap_4/new_chap_4/4_2_1/main.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
-#include "read_line.h"
+#include <stdlib.h>
+#include "readline.h"
 
 int main(int argc, const char* argv[])
 {
-	char *myline;
-	read_line(stdin, &myline);
+    FILE* fp = fopen("haha.txt", "r");
+    if(!fp) {
+        perror("File opening failed");
+        return EXIT_FAILURE;
+    }
+	char *myline = read_line(fp);
 	printf("%s\n", myline);
+    if (ferror(fp))
+        puts("I/O error when reading");
+    else if (feof(fp))
+        puts("End of file reached successfully");
+
+    fclose(fp);
 	return 0;
 }
